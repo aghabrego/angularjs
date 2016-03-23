b8140f3414bc569ffd3ef60af39bdb57a039a95c
Angel Hidalgo <you@example.com>
2016-03-23 20:35:54 +0000

    Agregando mini sistema de comentarios

--- controller.js
+++ controller.js
@@ -1,7 +1,26 @@
 // Creacion de mudulo
 var app = angular.module("myFirstApp", []);
+// ngResource consume app REST
 
 // Creacion de controlador
-app.controller("firstController", function($scope) { // Objeto scope
-	$scope.nombre = "Angel";
-});
+app.controller("firstController", ["$scope", function(e) { // Objeto scope
+	e.nombre = "Angel";
+	e.nuevoComentario = {};
+	e.comentrios = [
+		{
+			comentrio: "Bue tutorial",
+			username: "codigofacilito"
+		},
+		{
+			comentrio: "Malisimo el tutorial",
+			username: "otros_usuarios"
+		}
+	];
+	e.agregarComentario = function () {
+		// Agrega un nuevo elemento
+		e.comentrios.push(e.nuevoComentario);
+
+		// Inicializar variable de scope
+		e.nuevoComentario = {};
+	}
+}]);
